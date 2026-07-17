a15fcc57b70c1b123d4aef9a0d8222d096321758
jsilverhand48 <57627031+jsilverhand48@users.noreply.github.com>
2026-07-17 16:38:29 -0500

    Feature, deleted deprecated deploy dir

--- deploy/initdb/01-init.sql
+++
@@ -1,7 +0,0 @@
--- Dev-only bootstrap for the Docker Compose Postgres.
-CREATE EXTENSION IF NOT EXISTS pgcrypto;
-CREATE EXTENSION IF NOT EXISTS citext;
-CREATE DATABASE hyproxy_test OWNER hyproxy;
-\connect hyproxy_test
-CREATE EXTENSION IF NOT EXISTS pgcrypto;
-CREATE EXTENSION IF NOT EXISTS citext;
